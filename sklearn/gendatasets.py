ca15f5ef419827fc694511ce174abc7080b199f3
pythian-ciciliani <ciciliani@pythian.com>
2017-07-01 10:19:16 -0300

    code cleanup, centroids plot and parameters for gendatasets.py

--- sklearn/gendatasets.py
+++ sklearn/gendatasets.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
 from sklearn import datasets
+import argparse
 
 # Genero un set det de datos
 ## make_blobs = nube de datos isotropica, gaussiana
-atributos = 2
-centros = 3
-data,tags = datasets.make_blobs(n_samples=100, n_features=atributos, centers=centros, cluster_std=1.0, center_box=(-10.0, 10.0), shuffle=True, random_state=None)
+
+
+parser = argparse.ArgumentParser(description='Create a ramdom data set')
+parser.add_argument('-a','--attributes', type=int, nargs='+',required=True,
+                           help='Atributes')
+parser.add_argument('-c','--centers', type=int, nargs='+',required=True,
+                           help='Centers')
+args = parser.parse_args()
+
+data,tags = datasets.make_blobs(n_samples=200, n_features=args.attributes[0], centers=args.centers[0], cluster_std=1.0, center_box=(-10.0, 10.0), shuffle=True, random_state=None)
 
 datasets.dump_svmlight_file(data,tags,'dataset.svl')
--- sklearn/k-means.py
+++ sklearn/k-means.py
@@ -7,88 +7,81 @@ from sklearn import datasets
 from sklearn.preprocessing import scale
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_svmlight_file
+from mpl_toolkits.mplot3d import Axes3D
 
-## Loading data from file
+## LOADING DATASET FROM FILE
+## *************************
+## dataspmat is a scipy matrix storing all the data points. 
+## and tags is a numpy.ndarray storing tags for each data point
 dataspmat,tags = datasets.load_svmlight_file('dataset.svl')
 
 ## Converting from SciPy Sparse matrix to numpy ndarray
 data = dataspmat.toarray()
 
-#Preprocessing values
-scaleddata = scale(data)
 
-## Extraigo la cantidad de clases del array de etiquetas
+## Extracting amount of groups by determining unique tags number
 n_digits = len(np.unique(tags))
 
-
+## Creating KMeans object to process the dataset
 kmeans = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
 
-print("Etiquetas originales")
-print("****************************")
-print(tags)
-print(" ")
-print(" ")
-
-print("Etiquetas generadas por el kmeans, con los datos sin preprocesar")
-print("****************************")
+## Compute k-means clustering against the original data set
 kmeans.fit(data)
-print(kmeans.labels_)
-print(" ")
-print(" ")
 
-print("Etiquetas generadas por el kmeans, con los datos preprocesados")
-print("****************************")
+## Save centroids for plotting
+datacentroids=kmeans.cluster_centers_
+
+## Preprocess data set and repeat k-means clustering
+scaleddata = scale(data)
 kmeans.fit(scaleddata)
-print(kmeans.labels_)
-print(" ")
-print(" ")
+scaleddatacentroids=kmeans.cluster_centers_
 
 
-# Step size of the mesh. Decrease to increase the quality of the VQ.
-h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
 
-# Plot the decision boundary. For that, we will assign a color to each
-x_min, x_max = data[:, 0].min() - 1, data[:, 0].max() + 1
-y_min, y_max = data[:, 1].min() - 1, data[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+## PLOTTING RESULTS
+## ******************************
 
-# Obtain labels for each point in mesh. Use last trained model.
-Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+print("<<<<<< 3D PLOT IS NOT WORKING AS ALL BLURBS ARE ON THE SAME PLANE")
 
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-#plt.figure(1)
-#plt.clf()
-#plt.imshow(Z, interpolation='nearest',
-#           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-#           cmap=plt.cm.Paired,
-#           aspect='auto', origin='lower')
+#fig, ax = plt.subplots()
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-plt.plot(data[:, 0], data[:, 1], 'k.', markersize=2)
-# Plot the centroids as a white X
-#centroids = kmeans.cluster_centers_
-#plt.scatter(centroids[:, 0], centroids[:, 1],
-#            marker='x', s=169, linewidths=3,
-#            color='w', zorder=10)
-#plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
-#          'Centroids are marked with white cross')
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
+fig.suptitle("Original and scaled data points with their correspondant K-Means centroids", fontsize=10)
+
+## Plot the original data points
+ax.plot(data[:, 0], data[:, 1], 'k.', markersize=2)
+
+## Plot the scaled data points
+ax.plot(scaleddata[:, 0], scaleddata[:, 1], 'k.', markersize=2,color='r')
+
+# Plot the centroids as a green X for both, the original and the scaled data set
+centroids = datacentroids
+ax.scatter(centroids[:, 0], centroids[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='g', zorder=10)
+centroids = scaleddatacentroids
+ax.scatter(centroids[:, 0], centroids[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='g', zorder=10)
+
+## Graph and axis formatting
+ax.set_aspect('equal')
+ax.grid(True, which='both')
+
+# set the x-spine
+ax.spines['left'].set_position('zero')
+
+# turn off the right spine/ticks
+ax.spines['right'].set_color('none')
+#ax.yaxis.tick_left()
+
+# set the y-spine
+ax.spines['bottom'].set_position('zero')
+
+# turn off the top spine/ticks
+ax.spines['top'].set_color('none')
+ax.xaxis.tick_bottom()
+
 plt.show()
-
-
-
-x_min, x_max = scaleddata[:, 0].min() - 1, data[:, 0].max() + 1
-y_min, y_max = scaleddata[:, 1].min() - 1, data[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-plt.plot(data[:, 0], data[:, 1], 'k.', markersize=2,color='r')
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
-plt.show()
-
-
 
