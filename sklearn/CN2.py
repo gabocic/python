7ff036bba94ee27f167263c954149a7ea89d2d8e
pythian-ciciliani <ciciliani@pythian.com>
2017-07-09 10:37:54 -0300

    CN2.py: rule to text - trying to convert 'float' tags into strings

--- sklearn/CN2.py
+++ sklearn/CN2.py
@@ -19,18 +19,44 @@ data = dataspmat.toarray()
 att1 = Orange.data.ContinuousVariable(name='att1', compute_value=None)
 att2 = Orange.data.ContinuousVariable(name='att2', compute_value=None)
 att3 = Orange.data.ContinuousVariable(name='att3', compute_value=None)
-classv = Orange.data.DiscreteVariable(name='classv', compute_value=None)
+#classv = Orange.data.DiscreteVariable(name='classv', compute_value=None)
+classv = Orange.data.StringVariable(name='classv', compute_value=None)
 
 # Create domain based on the above attributes
 mydomain = Orange.data.Domain(attributes=[att1,att2,att3],class_vars=classv)
 
+charar = np.chararray((100, 1))
+
+for idx,etiq in np.ndenumerate(tags):
+	if etiq == 0:
+		charar[idx]='A'
+	elif etiq == 1:
+		charar[idx]='B'
+	elif etiq == 2:
+		charar[idx]='C'
+
+
 ## Loading data and tags in ndarray format into a an Orange.Table
-table = Orange.data.Table.from_numpy(mydomain,data,Y=tags)
+table = Orange.data.Table.from_numpy(mydomain,data,Y=charar)
 
 # construct the learning algorithm and use it to induce a classifier
-cn2_learner = Orange.classification.rules.CN2Learner()
-cn2_classifier = cn2_learner(table)
 
-# All rule-base classifiers can have their rules printed out like this:
-#for r in cn2_classifier.rules:
-#    print(Orange.classification.rules.rule_to_string(r))
+learner = Orange.classification.CN2Learner()
+
+# consider up to 10 solution streams at one time
+learner.rule_finder.search_algorithm.beam_width = 10
+
+# continuous value space is constrained to reduce computation time
+learner.rule_finder.search_strategy.constrain_continuous = True
+
+# found rules must cover at least 15 examples
+learner.rule_finder.general_validator.min_covered_examples = 15
+
+# found rules may combine at most 2 selectors (conditions)
+learner.rule_finder.general_validator.max_rule_length = 2
+
+classifier = learner(data)
+
+for myrule in classifier.rule_list:
+	print(myrule.__str__())
+
