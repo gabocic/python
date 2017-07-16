4e3a497b0869258fb8a0fcbd3e629956b4f543ff
pythian-ciciliani <ciciliani@pythian.com>
2017-07-15 21:51:53 -0300

    Making CART.py to print rules

--- sklearn/CART.py
+++ sklearn/CART.py
@@ -1,3 +1,5 @@
+#!/home/gabriel/pythonenvs/v3.5/bin/python
+
 from sklearn.datasets import load_iris
 from sklearn import tree
 from sklearn.tree import _tree
@@ -14,20 +16,20 @@ def tree_to_code(tree, feature_names):
         feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
         for i in tree_.feature
     ]
-    print "def tree({}):".format(", ".join(feature_names))
+    #print("def tree({}):".format(", ".join(feature_names)))
 
     def recurse(node, depth):
         indent = "  " * depth
         if tree_.feature[node] != _tree.TREE_UNDEFINED:
             name = feature_name[node]
             threshold = tree_.threshold[node]
-            print "{}if {} <= {}:".format(indent, name, threshold)
+            print("{}if {} <= {}:".format(indent, name, threshold))
             recurse(tree_.children_left[node], depth + 1)
-            print "{}else:  # if {} > {}".format(indent, name, threshold)
+            print("{}else:  # if {} > {}".format(indent, name, threshold))
             recurse(tree_.children_right[node], depth + 1)
         else:
-            print "{}return {}".format(indent, tree_.value[node])
+            print("{}return {}".format(indent, tree_.value[node]))
 
     recurse(0, 1)
 
-tree_to_code(tree,
+tree_to_code(clf,iris.feature_names)
