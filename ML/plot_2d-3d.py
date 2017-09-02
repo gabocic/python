994a11665e7980d6636f33ca15249a581063b569
pythian-ciciliani <ciciliani@pythian.com>
2017-09-02 08:37:42 -0300

    Final working plot_2d-3d.py, including Gaussian noise

--- ML/plot_2d-3d.py
+++ ML/plot_2d-3d.py
@@ -6,32 +6,38 @@ from mpl_toolkits.mplot3d import Axes3D
 
 generator = np.random
 
-n_features=3
-p0 = np.array(generator.random_integers(low=(0), high=10, size=(n_features)))
-p1 = np.array(generator.random_integers(low=(0), high=10, size=(n_features)))
+n_features=5
+#p0 = np.array(generator.random_integers(low=(0), high=10, size=(n_features)))
+p0 = generator.randn(n_features)
+#p1 = np.array(generator.random_integers(low=(0), high=10, size=(n_features)))
+p1 = generator.randn(n_features)
 
 d0 = np.array(p1 - p0)
 
+points = np.zeros((15,n_features))
+for a in range(1,15):
+    lins = p0+a*d0
+    # Add some noise 
+    lins += np.random.normal(size=lins.shape) * 0.4
+    points[a:a+1,:] = lins
+print(points)
 
 ## PLOTTING RESULTS
 ## ******************************
 
 if n_features == 2:
     fig,ax = plt.subplots()
-else:
+    ax.scatter(points[:,0],points[:,1],color='b')
+elif n_features == 3:
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d') # <-- 3D
+    ax.scatter(points[:,0],points[:,1],points[:,2],color='b')
 
 # SubTitle
 fig.suptitle("Dataset linear points", fontsize=10)
 
-## Plot the original data points
-for a in range(1,9):
-    lins = p0+a*d0
-    print(lins)
-    ax.scatter(*lins,color='b') # <-- 3D
-ax.scatter(*p0, color='g') # <-- 3D
-ax.scatter(*p1, color='g') # <-- 3D
+ax.scatter(*p0, color='g')
+ax.scatter(*p1, color='g')
 
 ## Graph and axis formatting
 ax.set_aspect('equal')
