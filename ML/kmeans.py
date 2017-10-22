efa62be6504e6ee64a01f914598f1de3b103962e
pythian-ciciliani <ciciliani@pythian.com>
2017-10-21 21:53:35 -0300

    k-means: centroids plotting - different types of centroid strategies

--- ML/kmeans.py
+++ ML/kmeans.py
@@ -1,17 +1,24 @@
 #!/home/gabriel/pythonenvs/v3.5/bin/python
 
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 from plot_2d_3d import plot_2d_3d
 
 
-def k_means_clustering(data):
+def k_means_clustering(data,plot=0,p_init='k-means++',p_n_clusters=3,p_n_init=10,p_n_jobs=2):
 
     ## Creating KMeans object to process the dataset
     ## **********************************************
     # k-means++ initialization scheme is specified (use the init='kmeans++' parameter), which has been implemented in scikit-learn. This initializes the centroids to be (generally) 
     # distant from each other, leading to probably better results than random initialization (init=random and PCA-based are two other possible choices). The parameter n_job specify 
     # the amount of processors to be used (default: 1). A value of -1 uses all available processors, with -2 using one less, and so on.
-    kmeans = KMeans(init='k-means++', n_clusters=4, n_init=10, n_jobs=2)
+
+    if p_init == 'PCA-based':
+        n_digits = len(np.unique(digits.target)) ## << Understand this! "digits" is making reference to the sklearn digits dataset
+        pca = PCA(n_components=n_digits).fit(data)
+        p_init=pca.components_
+
+    kmeans = KMeans(init=p_init, n_clusters=p_n_clusters, n_init=p_n_init, n_jobs=p_n_jobs)
 
     ## Compute k-means clustering against the original data set
     kmeans.fit(data)
@@ -19,24 +26,14 @@ def k_means_clustering(data):
     ## Save centroids for plotting
     centroids=kmeans.cluster_centers_
 
-    print(centroids)
+    if plot == 1:    
+        element_list=[]
+        element={'type':'dot','value':centroids.T,'color':'g','marker':'x','size':90}
+        element_list.append(element)
 
-    ## PLOTTING RESULTS
-    ## ******************************
+    #    element={'type':'dot','value':p1,'color':'g','marker':'x','size':90}
+    #    element_list.append(element)
 
-#    element={'type':'dot','value':p1,'color':'g','marker':'x','size':90}
-#    element_list.append(element)
-    
-#    element={'type':'dot','value':p1,'color':'g','marker':'x','size':90}
-#    element_list.append(element)
+        plot_2d_3d(element_list,3)
 
-
-#    ax.plot(data[:, 0], data[:, 1], data[:, 2],'k.', markersize=2) # <-- 3D
-
-#    ax.scatter(centroids[:, 0], centroids[:, 1], centroids[:, 2], # <-- 3D
-#                marker='x', s=169, linewidths=3,
-#                color='g', zorder=10)
-
-#    ax.scatter(centroids[:, 0], centroids[:, 1], centroids[:, 2], # <-- 3D
-#                marker='x', s=169, linewidths=3,
-#                color='g', zorder=10)
+    return kmeans.labels_
--- ML/runTest.py
+++ ML/runTest.py
@@ -11,16 +11,21 @@ def main():
     dataset = create_dataset(n_samples=100, n_features=3,
                         perc_lin=20, perc_repeated=0, n_groups=2,
                         avg_sample_dist=1.0, shift=0.0, scale=1.0, perc_feat_lin_dep=0,
-                        shuffle=True,feat_dist=0,debug=0,plot=1,save_to_file=0)
+                        shuffle=True,feat_dist=0,debug=0,plot=0,save_to_file=0)
 
     # Validate dataset is within the specifications
-    analyze_dataset(data=dataset,debug=0,plot=1,load_from_file=None)
+    analyze_dataset(data=dataset,debug=0,plot=0,load_from_file=None)
 
     # Scale data
     scaleddata = sklearn_scale(dataset) 
 
     # Find clusters using K-means algorithm
-    k_means_clustering(scaleddata)
+    #labels = k_means_clustering(scaleddata,0)
+    labels = k_means_clustering(data=scaleddata,plot=1,p_init='k-means++',p_n_clusters=3,p_n_init=10,p_n_jobs=4)
+    
+    print(labels)
+
+
 
 if __name__ == '__main__':
     main()
