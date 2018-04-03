c5ad06f45f26c59d9950e02a0319964ec11610f0
Gabriel Ciciliani <gabriel.ciciliani@gmail.com>
2018-04-03 01:17:24 -0300

    K-means: determining best K

--- ML/kmeans.py
+++ ML/kmeans.py
@@ -7,6 +7,7 @@ from plot_2d_3d import plot_2d_3d
 from metrics import clustering_metrics
 from common import split_data_in_clusters
 from collections import Counter
+import numpy as np
 
 def k_means_clustering(data,plot,p_init,p_n_clusters,p_n_init,p_n_jobs):
 
@@ -50,51 +51,79 @@ def k_means_clustering(data,plot,p_init,p_n_clusters,p_n_init,p_n_jobs):
             print('Cluster '+singleclus.__str__()+':',len(clusters[singleclus]))
 
         
-        # Calculate metrics. To solve ties, I'm prioritizing runs with less amount of clusters
-        clus_metrics = clustering_metrics(cleanlabels, 'k-means_'+n_clusters.__str__()+'clus',cleandata, elap_time, None, clusters,sin_ele_clus)
-        
-        # Find the highest metric values. To solve ties, I'm prioritizing runs with less amount of clusters
-        if n_clusters > l_clus_range:
-            if clus_metrics['silhouette_score'] > metrics_win_val[0]:
-                metrics_winners[0] = n_clusters
-                metrics_win_val[0] = clus_metrics['silhouette_score']
-            
-            if clus_metrics['calinski_harabaz_score'] > metrics_win_val[1]:
-                metrics_winners[1] = n_clusters
-                metrics_win_val[1] = clus_metrics['calinski_harabaz_score']
-            
-            if clus_metrics['dunn_index'] > metrics_win_val[2]:
-                metrics_winners[2] = n_clusters
-                metrics_win_val[2] = clus_metrics['dunn_index']
-            
-            if clus_metrics['sin_ele_clus'] < metrics_win_val[3]:
-                metrics_winners[3] = n_clusters
-                metrics_win_val[3] = clus_metrics['sin_ele_clus']
-            if clus_metrics['sin_ele_clus'] > 0:
-                flag_sel=1
+        ### As per the "Investigation of Internal Validity Measures for K-Means Clustering" paper, the Sum-of-squares method was found to be the most effective for predicting the 'best' number of clusters        ### To calculate the Sum-of-Squares index we need to calculate the Sum-of-Squares 'within the clusters' (SSW) and the Sum-of-squares 'between the clusters' (SSB)
 
-        else:
-            metrics_winners[0] = n_clusters
-            metrics_win_val[0] = clus_metrics['silhouette_score']
-            metrics_winners[1] = n_clusters
-            metrics_win_val[1] = clus_metrics['calinski_harabaz_score']
-            metrics_winners[2] = n_clusters
-            metrics_win_val[2] = clus_metrics['dunn_index']
-            metrics_winners[3] = n_clusters
-            metrics_win_val[3] = clus_metrics['sin_ele_clus']
-        
-        # Save metrics 
-        all_metrics[n_clusters] = clus_metrics
-        
-        print(metrics_winners)
-        print(metrics_win_val)
+        ## SSW
+        ssac = 0
+        clustermeans = []
+        for clusterid in clusters:
+            # Save cluster mean for SSB calculation
+            clustermean = clusters[clusterid].mean()
+            clustermeans.append(clustermean)
 
-        # If no iteration generated single element clusters, do not consider this metric 
-    if flag_sel == 0:
-        del metrics_winners[3]
-        del metrics_win_val[3]
-    ocurrences = Counter(metrics_winners)
-    print(ocurrences)
+            # For each cluster, calculate the sum of squares
+            ssc = np.sum((clusters[clusterid]-clustermean)**2
+
+            # Add all partial sum-of-squares
+            ssac += ssc
+
+        # Calculate Sum-of-Squares Within as the average between all Sum-of-Squares
+        ssw = ssac/cleandata.shape[0]
+        print('ssw',ssw)
+
+        ## SSB
+        dscentroid = cleandata.mean()
+        clustermeans = np.asarray(clustermeans)
+        dist = (clustermeans - dscentroid)**2
+
+
+
+#        # Calculate metrics. To solve ties, I'm prioritizing runs with less amount of clusters
+#        clus_metrics = clustering_metrics(cleanlabels, 'k-means_'+n_clusters.__str__()+'clus',cleandata, elap_time, None, clusters,sin_ele_clus)
+#        
+#        # Find the highest metric values. To solve ties, I'm prioritizing runs with less amount of clusters
+#        if n_clusters > l_clus_range:
+#            if clus_metrics['silhouette_score'] > metrics_win_val[0]:
+#                metrics_winners[0] = n_clusters
+#                metrics_win_val[0] = clus_metrics['silhouette_score']
+#            
+#            if clus_metrics['calinski_harabaz_score'] > metrics_win_val[1]:
+#                metrics_winners[1] = n_clusters
+#                metrics_win_val[1] = clus_metrics['calinski_harabaz_score']
+#            
+#            if clus_metrics['dunn_index'] > metrics_win_val[2]:
+#                metrics_winners[2] = n_clusters
+#                metrics_win_val[2] = clus_metrics['dunn_index']
+#            
+#            if clus_metrics['sin_ele_clus'] < metrics_win_val[3]:
+#                metrics_winners[3] = n_clusters
+#                metrics_win_val[3] = clus_metrics['sin_ele_clus']
+#            if clus_metrics['sin_ele_clus'] > 0:
+#                flag_sel=1
+#
+#        else:
+#            metrics_winners[0] = n_clusters
+#            metrics_win_val[0] = clus_metrics['silhouette_score']
+#            metrics_winners[1] = n_clusters
+#            metrics_win_val[1] = clus_metrics['calinski_harabaz_score']
+#            metrics_winners[2] = n_clusters
+#            metrics_win_val[2] = clus_metrics['dunn_index']
+#            metrics_winners[3] = n_clusters
+#            metrics_win_val[3] = clus_metrics['sin_ele_clus']
+#        
+#        # Save metrics 
+#        all_metrics[n_clusters] = clus_metrics
+#        
+#        print(metrics_winners)
+#        print(metrics_win_val)
+#
+#        # If no iteration generated single element clusters, do not consider this metric 
+#    if flag_sel == 0:
+#        del metrics_winners[3]
+#        del metrics_win_val[3]
+#    #ocurrences = Counter(metrics_winners)
+#    most_common,num_most_common = Counter(metrics_winners).most_common(1)[0] # 4, 6 times
+#    print(most_common,num_most_common)
 
 
 #    for metric_group in all_metrics:
